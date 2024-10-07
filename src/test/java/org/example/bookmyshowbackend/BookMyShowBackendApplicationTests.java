02fe614497c6cf685248f834e44cc18cf3172b25
Gowtham <gowthamt1997@gmail.com>
2024-10-07 11:17:48 +0530

    Project Initiated

---
+++ src/test/java/org/example/bookmyshowbackend/BookMyShowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookmyshowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
